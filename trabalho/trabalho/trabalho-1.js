c521051ad9a68319585ba537915c985982578738
arthur da silva <101300094+thurssl@users.noreply.github.com>
2023-06-14 22:05:12 -0300

    trabalho 2

---
+++ trabalho/trabalho/trabalho-1.js
@@ -0,0 +1,26 @@
+ const nome = "Arthur silva"
+
+ const nota = 9
+
+ if (nota >= 7){
+     console.log("O aluno "+nome+" está aprovado");
+ } else {
+     console.log("O aluno "+nome+ " está reprovado")
+ }
+
+ const nomeAlu = "Arthur silva"
+const idadeAlu = "17"
+ const cursoAlu = "TI"
+const nota1 = 9
+ const nota2 = 8
+
+console.log("O aluno "+nomeAlu+ " tem "+idadeAlu+" anos, cursa "+cursoAlu+" e suas notas foram "+nota1+ " e "+nota2);
+const cliente = {
+    nome: "Arthur silva",
+    receita: 4000.00,
+    despesas: 3153.00
+}
+
+const saldoCliente = cliente.receita - cliente.despesas
+
+console.log("O cliente " +cliente.nome+ " possui o saldo de R$ "+saldoCliente)
